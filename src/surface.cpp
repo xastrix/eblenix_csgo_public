bc0c3bc7ab84950fb7606619fcdf989de7053f84
xastrix <xxxxastrixxxx@gmail.com>
2026-03-10 04:13:48 +0300

    fill c_surface_draw_manager

--- src/surface.cpp
+++ src/surface.cpp
@@ -1,1 +1,55 @@
 #include "surface.h"
+
+void c_surface_draw_manager::set_draw_color(int r, int g, int b, int a)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int, int, int, int);
+	return (*(original_fn**)this)[15](this, r, g, b, a);
+}
+
+void c_surface_draw_manager::draw_filled_rect(int x, int y, int w, int h)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int, int, int, int);
+	return (*(original_fn**)this)[16](this, x, y, x + w, y + h);
+}
+
+void c_surface_draw_manager::draw_outlined_rect(int x, int y, int w, int h)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int, int, int, int);
+	return (*(original_fn**)this)[18](this, x, y, x + w, y + h);
+}
+
+void c_surface_draw_manager::draw_line(int x1, int y1, int x2, int y2)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int, int, int, int);
+	return (*(original_fn**)this)[19](this, x1, y1, x2, y2);
+}
+
+void c_surface_draw_manager::get_screen_size(int& width, int& height)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int&, int&);
+	return (*(original_fn**)this)[44](this, width, height);
+}
+
+void c_surface_draw_manager::unlock_cursor()
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*);
+	return (*(original_fn**)this)[66](this);
+}
+
+void c_surface_draw_manager::lock_cursor()
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*);
+	return (*(original_fn**)this)[67](this);
+}
+
+void c_surface_draw_manager::play_sound(const char* path)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, const char*);
+	return (*(original_fn**)this)[82](this, path);
+}
+
+void c_surface_draw_manager::get_cursor_pos(int& x, int& y)
+{
+	using original_fn = void(__thiscall*)(c_surface_draw_manager*, int&, int&);
+	return (*(original_fn**)this)[100](this, x, y);
+}
--- src/surface.h
+++ src/surface.h
@@ -2,7 +2,15 @@
 
 #define VGUI_SURFACE_INTERFACE_VERSION "VGUI_Surface031"
 
-class c_surface_draw_manager/* {
+class c_surface_draw_manager {
 public:
-
-}*/;
+	void set_draw_color(int r, int g, int b, int a = 255);
+	void draw_filled_rect(int x, int y, int w, int h);
+	void draw_outlined_rect(int x, int y, int w, int h);
+	void draw_line(int x1, int y1, int x2, int y2);
+	void get_screen_size(int& width, int& height);
+	void unlock_cursor();
+	void lock_cursor();
+	void play_sound(const char* path);
+	void get_cursor_pos(int& x, int& y);
+};
